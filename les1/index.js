ba64d5e5f41d54552b14bbcd52e596b175024360
kalashovkerim <kkalashov.unity@gmail.com>
2023-10-08 16:11:02 +0400

    les1 done

---
+++ les1/index.js
@@ -0,0 +1,19 @@
+import { format, formatDistance, formatRelative, subDays } from 'date-fns'
+import _ from 'lodash'
+
+let res = format(new Date(), "'Today is a' eeee")
+//=> "Today is a Sunday"
+
+console.log(res);
+
+const myList = [
+    1,
+    2,
+    3,
+    4,
+    5
+]
+
+const newArr = _.chunk(myList, 3)
+
+console.log(newArr);
